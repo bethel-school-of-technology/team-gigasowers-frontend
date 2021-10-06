a6eec6a8bea78abd27afd8fc7783b0aa01ebcffe
Daniel L Gliniak <daniellgliniak@Mac-mini.local>
2021-10-06 14:43:20 -0500

    adding moass

--- veggie-tender/src/components/Navbar.js
+++ veggie-tender/src/components/Navbar.js
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import "../../src/App.css";
+import ReorderIcon from '@material-ui/icons/Reorder';
+import SearchIcon from '@material-ui/icons/Search';
+
+import LoginForm from "./LoginForm"
+import { Route } from 'react-router';
 
 function Navbar() {
     const[showLinks, setShowLinks] = useState(false);
@@ -7,16 +12,25 @@ function Navbar() {
         <div className="Navbar">
             <div className="leftSide">
                 <div className="links">
-                    <a href="/home">Home</a>
+                    <a href="/home" id={showLinks ? "hidden" : ""}>Home</a>
                     <a href="/farmers">Farmers</a>
                     <a href="/events">Events</a>
                     <a href="/Umm">MAYYYYYYBEEEE</a>
-                    <button> Button </button>
                 </div>
+                <button onClick={()=> setShowLinks(!showLinks)}>
+                        {" "} 
+                        <ReorderIcon/>
+                </button>
             </div>
             <div className="reftSide">
                 <input type="text" placeholder="search"/>
-                <button> Search </button>
+                {/* <button> <SearchIcon/> </button>
+                    var button = document.getElementById('myButton');
+                    button.onclick = function() {
+                    location.assign('/users/login')
+                    }
+                <button id="myButton">Visit Website</button> */}
+                <button><Route exact path="/" component={LoginForm}>Login</Route></button>
             </div>
         </div>
     )
