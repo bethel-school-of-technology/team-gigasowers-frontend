4f9cc38b925a110aa0602c110c08a2cb466374a8
Daniel L Gliniak <daniellgliniak@Mac-mini.local>
2021-10-09 13:38:18 -0500

    landing page, pls check to see if displayed on main local 3k

--- veggie-tender/src/pages/Landing.js
+++ veggie-tender/src/pages/Landing.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+
 
 export default function Landing() {
     return (
-        <div>
-            
+        <div className="landing">
+            <div>
+            <h1>This is Landing</h1>
+            <p>ああああああああああああああああああああああああああああああ</p>
+            </div>
+            <div className="landingComponents">
+                <div>
+                    thinking
+                </div>
+                <div>
+                    of
+                </div>
+                <div>
+                    how 
+                </div>
+                <div>
+                    to 
+                </div>
+                <div>
+                    put
+                </div>
+                <div>
+                    together
+                </div>
+                <div>
+                    maybe I build another component to fill inside the landing page
+                </div>
+                <div>
+                    and Ill import that to be displayed.....hmmmmm.
+                </div>
+            </div>
         </div>
     )
 }
