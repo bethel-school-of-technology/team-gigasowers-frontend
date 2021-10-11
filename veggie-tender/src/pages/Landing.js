bcd69a061cc7a0b054751cd09ec84c379fbd984d
Ben Young <ben.young.project@gmail.com>
2021-10-11 14:25:04 -0500

    back on track?

--- veggie-tender/src/pages/Landing.js
+++ veggie-tender/src/pages/Landing.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LandingFunction from '../components/LandingFunction';
 
 export default function Landing() {
     return (
         <div>
-            
+            <LandingFunction />
         </div>
     )
 }
