86d5431c012b9ae746aa4db11246c2bc0a040ea4
Daniel L Gliniak <daniellgliniak@Mac-mini.local>
2021-10-20 15:08:27 -0500

    landing working

--- veggie-tender/src/components/Landing/Card.js
+++ veggie-tender/src/components/Landing/Card.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'; //{useState, useEffect}
 import styled from 'styled-components';
 //import ReactPlaceholder from 'react-placeholder';
 //import "react-placeholder/lib/reactPlaceholder.css";
 import { CustomPlaceholder } from 'react-placeholder-image';
-import farmName from './LandingFunction';
-import farmDescription from './LandingFunction';
+//import farmName from './LandingFunction';
+//import farmDescription from './LandingFunction';
+//import farmId from './LandingFunction';
 
 const CardStyles = styled.div`
-
 .card{
    flex: 0.8;
    box-sizing: border-box;
@@ -27,6 +28,7 @@ const CardStyles = styled.div`
     justify-content: space-between;
     cursor: pointer;
     transition: transform 200ms ease-in;
+
 }
 
 .cardImg{
@@ -70,7 +72,68 @@ color: white;
 }
 `;
 
+//const responce = "";
+
 export default function Card() {
+
+    const [user, setUser] = useState([]);
+
+
+    useEffect(() => {
+       getFarm();
+    }, [])
+
+
+
+    async function getFarm() {
+        try {
+        const response = await axios.get('http://localhost:5000/api/users/farms');
+    
+        console.log(response.data)
+        setUser(response.data)
+
+        //   for (let i = 0; i < response.length; i++) {
+        //       console.log(response[i].userF)
+        //   }
+        //  response = await axios.get('/farms.farmDescription');
+        //   console.log(response);
+        //  response = await axios.get('/farms.farmId');
+        //   console.log(response);
+        } catch (error) {
+        console.error(error);
+        }
+    }
+
+    return (
+        <CardStyles>
+
+            {user.map(currentUser => (  
+                <div>
+                <div className="cardWrapper">
+                    <div className="cardBody">
+                        <div className="cardImg">
+                            {/* <img src="./imgtest/imgholder.png" /> */}
+                            <CustomPlaceholder width={265} height={149} />
+                        </div>
+                        <div className="cardWords">
+                            <p className ="cardFarmId">{currentUser.userFarms.farmId}</p>
+                            <h2 className="cardTitle">{currentUser.userFarms.farmName}</h2>
+                            <p className="cardDes">{currentUser.userFarms.farmDescription}</p>
+                        </div>
+                        </div>
+                    </div>
+                </div> 
+            ))};
+        </CardStyles>
+
+    )
+
+}
+
+//<button className="cardBut">Buttonです</button>
+//<div>{currentUser.userFarms.farmId}</div>
+
+
     // farmName = 'Farm Name',
     // farmDescription = 'Farm Details',
     // farmAddress = 'Farm Address',
@@ -80,31 +143,53 @@ export default function Card() {
     // farmEmail = "Farm Email",
 
 // }){
+    // const [repo, setRepo] = useState([]);
+    
 
-    return (
-        <CardStyles>
-            
-            <div className="cardWrapper">
+    // const getRepo = () => {
+    // axios.get('localhost:5000/api/users/farms')
+    // .then((response) => {
+    //     console.log(response);
+    //     const myRepo = response.data;
+    //     setRepo(myRepo);
+    // });
+    // };
 
-            <div className="cardBody">
-                <div className="cardImg">
-                    {/* <img src="./imgtest/imgholder.png" /> */}
-                    <CustomPlaceholder width={265} height={149} />
-                </div>
-                <div className="cardWords">
-                    <h2 className="cardTitle">{farmName}</h2>
-                    {/* instead of description, should i fill that w/ maybe first produce, maybe leave out?? */}
-                    <p className="cardDes">{farmDescription}</p>
-                </div>
-            </div>
-            </div>    
-        </CardStyles >
+    // useEffect (() => getRepo(), []);
 
 
-    )
+    // farmId = 'farm ID',
+    // farmName = 'Farm Name',
+    // farmDescription = 'Farm Details',
+    // farmCity = "Farm City",
 
-}
+    // function componentDidMount() {
+    //     fetch('localhost:5000/api/users/farms')
+    //         .then(response => response.json())
+    //         .then(data => this.setState({ totalReactPackages: data.total }));
+    // }
 
-//<button className="cardBut">Button</button>
+    // useEffect(() => {
+    //     // GET request using fetch inside useEffect React hook
+    //     fetch('https://localhost:5000/api/users/farms')
+    //         .then(response => response.json())
+    //         .then(data => setTotalReactPackages(data.total));
+    // }, []);
 
+    // axios.get('/farms', {
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
 
+//     axios.get('localhost:5000/api/users/farms')
+//   .then((response) => {
+    
+//     console.log(response);
+//   })
+//   .catch((error) => {
+  
+//   })
